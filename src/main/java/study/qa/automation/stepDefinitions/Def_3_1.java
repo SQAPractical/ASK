9c3081576a5947fa298b762f86c92a85b5313de6
AnaBK <lien82@gmail.com>
2019-02-05 22:55:44 -0500

    added 3_1

---
+++ src/main/java/study/qa/automation/stepDefinitions/Def_3_1.java
@@ -0,0 +1,51 @@
+package study.qa.automation.stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+
+public class MyStepdefs {
+    @And("^Push Register Now link$")
+    public void pushRegisterNowLink() {
+    }
+
+    @And("^Type First Name  \"([^\"]*)\"$")
+    public void typeFirstName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Type Last Name \"([^\"]*)\"$")
+    public void typeLastName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Type Email \"([^\"]*)\"$")
+    public void typeEmail(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Type Group Code \"([^\"]*)\"$")
+    public void typeGroupCode(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Type Confirm password \"([^\"]*)\"$")
+    public void typeConfirmPassword(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Push Register Me button$")
+    public void pushRegisterMeButton() {
+        
+    }
+
+    @And("^Delete Student with name \"([^\"]*)\"$")
+    public void deleteStudentWithName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
--- src/main/resources/features/Ana.feature
+++ src/main/resources/features/Ana.feature
@@ -1,1 +1,18 @@
-fgkdjglkgjlk
+Feature:  3.1
+  @EndToEnd
+  Scenario: Create User
+    When Navigate to login page
+    And Push Register Now link
+    And Type First Name  "Anton"
+    And Type Last Name "Antonov"
+    And Type Email "anton@antonov.com"
+    And Type Group Code " 123456"
+    And Type Password " 123456"
+    And Type Confirm password " 123456"
+    And Push Register Me button
+    #  // Login as teacher and delete student
+    And Type email "tanyasptest+6@gmail.com"
+    And Type Password "winter2018"
+    And Push Sign In button
+    And Delete Student with name "Anton Antonov"
+    And Click Logout on left navigational panel
