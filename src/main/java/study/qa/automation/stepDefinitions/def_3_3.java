557841a68e26c20e78e2941b076c01aac739351c
anurhan <anurkhanov@yahoo.com>
2019-02-06 11:07:26 -0600

    added stepdefiniitions

---
+++ src/main/java/study/qa/automation/stepDefinitions/def_3_3.java
@@ -0,0 +1,9 @@
+package study.qa.automation.stepDefinitions;
+
+import cucumber.api.java.en.And;
+
+public class def_3_3 {
+    @And("^Select Textual Type of question$")
+    public void selectTextualTypeOfQuestion() {
+    }
+}
