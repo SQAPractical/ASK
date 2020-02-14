e66d69cc12e06bf18b2a421d4412d804fd696c98
YevheniiS <sosonnyi@yahoo.com>
2020-02-14 12:55:22 -0600

    add .....

--- src/test/java/definitions/userLastName.java
+++ src/test/java/definitions/userLastName.java
@@ -1,22 +1,27 @@
 package definitions;
+
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Wait;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
-public class userFirstName {
+
+
+public class userLastName {
     @Given("I open login page")
     public void iOpenLoginPage() {
         getDriver().get("http://ask-stage.portnov.com/#/login");
     }
+
     @Given("I open registration page")
     public void iOpenRegistrationPage() {
         getDriver().get("http://ask-stage.portnov.com/#/registration");
     }
+
     @And("I type first name {string}")
     public void iTypeFirstName(String FName) {
         getDriver().findElement(By.xpath("//*[@placeholder='First Name']")).sendKeys(FName);
@@ -24,26 +29,30 @@ public class userFirstName {
 
     @And("I type last name {string}")
     public void iTypeLastName(String LName) {
-     getDriver().findElement(By.xpath("//*[@placeholder='Last Name']")).sendKeys(LName);
+        getDriver().findElement(By.xpath("//*[@placeholder='Last Name']")).sendKeys(LName);
     }
 
     @And("I type email {string}")
-    public void iTypeEmail(String mail) {
-        getDriver().findElement(By.xpath("//*[@placeholder='Email']")).sendKeys(mail);    }
+    public void iTypeEmail(String email) {
+        getDriver().findElement(By.xpath("//*[@placeholder='Email']")).sendKeys(email);
+    }
 
     @And("I type group code {string}")
-    public void iTypeGroupCode(String group) {
-        getDriver().findElement(By.xpath("//*[@placeholder='Group Code']")).sendKeys(group);
+    public void iTypeGroupCode(String Group) {
+        getDriver().findElement(By.xpath("//*[@placeholder='Group Code']")).sendKeys(Group);
+
     }
 
     @And("I type password {string}")
     public void iTypePassword(String pass) {
         getDriver().findElement(By.xpath("//*[@placeholder='Password']")).sendKeys(pass);
+
     }
 
     @And("I confirm password {string}")
-    public void iConfirmPassword(String ConPass) {
-    getDriver().findElement(By.xpath("//*[@placeholder='Confirm Password']")).sendKeys(ConPass);
+    public void iConfirmPassword(String ConfPass) {
+        getDriver().findElement(By.xpath("//*[@placeholder='Confirm Password']")).sendKeys(ConfPass);
+
     }
 
     @And("I click Register me button")
@@ -53,10 +62,10 @@ public class userFirstName {
 
     @Then("{string} message appears")
     public void messageAppears(String message) {
+
         WebElement element = getDriver().findElement(By.xpath("//*[contains(text(),'"+message+"')]"));
         assertThat(element.isDisplayed()).isTrue();
     }
-
 
     @And("I wait for {int} sec")
     public void iWaitForSec(int time) throws Throwable  {
--- src/test/resources/Features/Objects/google.feature
+++
@@ -1,5 +0,0 @@
-Feature: Yandex
-
-  Scenario: Open "Yandex and search.
-    Given I open url "www.yandex.com"
-    Then I cl
