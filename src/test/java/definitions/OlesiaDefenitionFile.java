0c127ceb5b24f5224c3a0c4c352179e38eeb325a
OlesiaKazmirchuk <oki100oki@gmail.com>
2020-03-26 00:51:39 -0700

    Updated

--- src/test/java/definitions/OlesiaDefenitionFile.java
+++ src/test/java/definitions/OlesiaDefenitionFile.java
@@ -1,7 +1,12 @@
 package definitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
@@ -21,4 +26,54 @@ public class OlesiaDefenitionFile {
     public void iShouldSeePageTitleContains(String pageTitle) {
         assertThat(getDriver().getTitle().contains(pageTitle));
     }
+
+    @When("I click on menu item {string}")
+    public void iClickOnMenuItem(String xpath) throws InterruptedException{
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath(xpath)).click();
+    }
+
+    @Then("element with xpath {string} should be displayed")
+    public void elementWithXpathShouldBeDisplayed(String xpath) {
+        assertThat(getDriver().findElement(By.xpath(xpath)).isDisplayed()).isTrue();
+    }
+
+    @Then("I wait for element with xpath {string} to be present")
+    public void iWaitForElementWithXpathToBePresent(String xpath) {
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+    }
+
+    @When("I type {string} into element with xpath {string}")
+    public void iTypeIntoElementWithXpath(String textToPut, String xpath) {
+        getDriver().findElement(By.xpath(xpath)).sendKeys(textToPut);
+    }
+
+
+    @Then("I wait for {int} sec")
+    public void iWaitForSec(int sec) throws Exception {
+        Thread.sleep(sec * 1000);
+    }
+
+    @And("I click on element with xpath {string}")
+    public void iClickOnElementWithXpath(String xpath) throws InterruptedException{
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath(xpath)).click();
+    }
+
+    @And("I type question {string}")
+    public void iTypeQuestion(String questionText) {
+        //label[contains(text(),'Question')]/..
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Question *']")).sendKeys(questionText);
+    }
+
+    @And("I type optionOne {string}")
+    public void iTypeOptionOne(String option) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).sendKeys(option);
+    }
+
+    @And("I type optionTwo {string}")
+    public void iTypeOptionTwo(String option) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 2*']")).sendKeys(option);
+    }
 }
+
--- src/test/resources/features/SingleChoiceQuestionText.feature
+++ src/test/resources/features/SingleChoiceQuestionText.feature
@@ -1,8 +1,32 @@
 Feature: Single Choice Question - Text
+
   Scenario: fill the field with Alphanumeric & Sp. characters
     Given I navigate to login page
     When I type email "vpmawork@gmail.com"
     And I type password "12345"
     And I click "Sign In" button
     Then I should see page title contains "Assessment Control"
+    When I click on menu item "//a[contains(@href,'quizzes')]"
+    Then I wait for element with xpath "//*[contains(text(),'List of Quizzes')]" to be present
+    Then element with xpath "//*[contains(text(),'List of Quizzes')]" should be displayed
+    When I click "Create New Quiz" button
+    Then I wait for element with xpath "//input[@formcontrolname='name']" to be present
+    When I type "ABC" into element with xpath "//input[@formcontrolname='name']"
+#    And I click "Add Question" button
+    And I click on element with xpath "//*[text()='add_circle']"
+ #   Then I wait for element with xpath "//*[contains(text(),'Single-Choice')]" to be present
+    And I click on menu item "//*[contains(text(),'Single-Choice')]"
+    And I type question "Are you happy?"
+    And I click on menu item "//textarea[@placeholder='Option 1*']/../../../../..//input"
+ # //textarea[@placeholder='Option 1*']/../../../../..//label/input  //input[contains(@name,'mat-radio-group')]   //div[@class='right']//input[@type='radio']
+    And I type optionOne "Yes"
+    And I type optionTwo "1234ABCD&&"
+    And I click "Save" button
+    Then I wait for 15 sec
+
+
+
+
+
+
 
