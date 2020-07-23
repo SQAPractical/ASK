5f0cfadefdad668f179e2f4fe484dd59c74fe931
lilian454 <63335841+lilian454@users.noreply.github.com>
2020-07-22 20:56:35 -0700

    added test.py file with methond 'test'

---
+++ test.py
@@ -0,0 +1,10 @@
+
+class ClassName(object):
+    """docstring for ."""
+
+        def __init__(self, arg):
+            super(, self).__init__()
+            self.arg = arg
+
+        def test(self):
+            pass
