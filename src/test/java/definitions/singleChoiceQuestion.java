e08d5216607c31dfa0608d5d611c44036017e38a
Atousa <testabc@yahoo.com>
2020-07-24 21:20:27 -0700

    Added Friday Task

---
+++ src/test/java/definitions/singleChoiceQuestion.java
@@ -0,0 +1,85 @@
+package definitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+
+import static support.TestContext.getDriver;
+
+public class quizzes {
+    @And("I click Quizzes in the left menu")
+    public void iClickInQuizzesButton() throws InterruptedException {
+        getDriver().findElement(By.xpath("//*[contains(text(),'Quizzes')]")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("I click Create New Quiz button")
+    public void iClickCreateNewQuizButton() {
+        getDriver().findElement(By.xpath("//span[contains(text(),'Create New Quiz')]")).click();
+    }
+
+    @And("I type {string} in Title Of The Quiz *")
+    public void iTypeInTitleOfTheQuiz(String Title) throws InterruptedException {
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//input[@placeholder='Title Of The Quiz *']")).sendKeys(Title);
+
+    }
+
+    @And("I click Add Question button")
+    public void iClickAddQuestionButton() throws InterruptedException {
+        getDriver().findElement(By.xpath("//mat-icon[@class='mat-icon material-icons']")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("I click Single-Choice radio button")
+    public void iClickSingleChoiceRadioButton() {
+        getDriver().findElement(By.xpath("//div[contains(text(),'Single-Choice')]")).click();
+    }
+
+    @And("I type {string} in Question *")
+    public void iTypeInQuestion(String Question) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Question *']")).sendKeys(Question);
+    }
+
+    @Then("I type {string} in Option one")
+    public void iTypeInOption(String Option1) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).sendKeys(Option1);
+    }
+
+    @Then("I click Option one radio button")
+    public void iClickOptionRadioButton() throws InterruptedException {
+        getDriver().findElement(By.xpath("//body/ac-root/mat-sidenav-container/mat-sidenav-content/main/ac-quiz-builder-page/form/main/mat-accordion/mat-expansion-panel/div/div/ac-question-body-form/div/div[2]/div[1]/mat-radio-group[1]/mat-radio-button[1]/label[1]/div[1]")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("I click preview button")
+    public void iClickPreviewButton() {
+        getDriver().findElement(By.xpath("//span[contains(text(),'Preview')]")).click();
+    }
+
+    @Then("Text field Other appears")
+    public void textFieldOtherAppears() {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Other']")).isDisplayed();
+    }
+
+    @And("I type {string} in Option two")
+    public void iTypeInOptionTwo(String Option2) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 2*']")).sendKeys(Option2);
+    }
+
+
+    @And("I click Include Other text area option for this question")
+    public void iClickIncludeOtherTextAreaOptionForThisQuestion() {
+        getDriver().findElement(By.xpath("//span[contains(text(),'Include \"Other\" text area option for this question')]")).click();
+    }
+
+    @And("I click close button")
+    public void iClickCloseButton() {
+        getDriver().findElement(By.xpath("//ac-quiz-preview[@class='ng-star-inserted']//button[@class='mat-raised-button mat-primary']")).click();
+    }
+
+    @And("I click Save button")
+    public void iClickSaveButton() {
+        getDriver().findElement(By.xpath("//ac-quiz-preview[@class='ng-star-inserted']//button[@class='mat-raised-button mat-primary']")).click();
+    }
+}
