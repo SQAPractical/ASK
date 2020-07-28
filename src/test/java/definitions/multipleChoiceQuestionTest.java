d71240edad7fd4576fd63c779db020542131aa33
nika202 <vdavidy1+1@gmail.com>
2020-07-28 15:06:21 -0700

    change

--- src/test/java/definitions/multipleChoiceQuestionTest.java
+++ src/test/java/definitions/multipleChoiceQuestionTest.java
@@ -9,39 +9,31 @@ public class multipleChoiceQuestionTest {
         getDriver().findElement(By.xpath("//*[contains(text(),'Quizzes')]")).click();
         Thread.sleep(2000);
     }
-    @And("I click Create New Quiz button")
-    public void iClickCreateNewQuizButton() throws InterruptedException {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Create New Quiz')]")).click();
-        Thread.sleep(2000);
-    }
+
     @And("I type {string} in the Title Of The Quiz *")
     public void iTypeInTheTitleOfTheQuiz(String Title) throws InterruptedException {
         getDriver().findElement(By.xpath("//input[@placeholder='Title Of The Quiz *']")).sendKeys(Title);
         Thread.sleep(2000);
     }
-    @And("I click Add Question button")
-    public void iClickAddQuestionButton() {
-        getDriver().findElement(By.xpath("//mat-icon[@class='mat-icon material-icons']")).click();
-    }
     @And("I click Multiple Choice radio button")
     public void iClickMultipleChoiceRadioButton() {
         getDriver().findElement(By.xpath("//div[contains(text(),'Multiple-Choice')]")).click();
     }
-    @Then("I type {string} in Question *")
-    public void iTypeInQuestion(String Question) throws InterruptedException {
-        getDriver().findElement(By.xpath("//*[contains(text(),'Q1')]/../../..//*[@placeholder='Question *']")).sendKeys(Question);
-        Thread.sleep(2000);
-    }
-    @Then("I type {string} in Option one")
-    public void iTypeInOptionOne(String Option1) throws InterruptedException {
-        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).sendKeys(Option1);
-        Thread.sleep(2000);
-    }
-    @Then("I type {string} in Option two")
-    public void iTypeInOptionTwo(String Option2) throws InterruptedException {
-        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 2*']")).sendKeys(Option2);
-        Thread.sleep(2000);
-    }
+//    @Then("I type {string} in Question *")
+//    public void iTypeInQuestion(String Question) throws InterruptedException {
+//        getDriver().findElement(By.xpath("//*[contains(text(),'Q1')]/../../..//*[@placeholder='Question *']")).sendKeys(Question);
+//        Thread.sleep(2000);
+//    }
+//    @Then("I type {string} in Option one")
+//    public void iTypeInOptionOne(String Option1) throws InterruptedException {
+//        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).sendKeys(Option1);
+//        Thread.sleep(2000);
+//    }
+//    @Then("I type {string} in Option two")
+//    public void iTypeInOptionTwo(String Option2) throws InterruptedException {
+//        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 2*']")).sendKeys(Option2);
+//        Thread.sleep(2000);
+//    }
     @Then("I click Option one")
     public void iClickOptionOne() {
         getDriver().findElement(By.xpath("//*[contains(text(),'Q1')]/../../..//*[@placeholder='Option 1*']/../../../../..//mat-checkbox/label")).click();
