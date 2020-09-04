f6a389e5ddb06ed97ab7484cfb2b41ca5c949156
AdamAly831 <adamaly831@gmail.com>
2020-09-04 15:45:16 -0700

    Test automation: Changing password

--- src/test/java/definitions/login.java
+++ src/test/java/definitions/login.java
@@ -83,7 +83,32 @@ public class login {
 
     }
 
+//password
+    @Then("I click on {string} on the side")
+    public void iClickOnOnTheSide(String settings) {
+        getDriver().findElement(By.xpath("//h5[contains(text(),'Settings')]")).click();
 
+    }
+
+    @Then("I Click {string} and I type in {string} in password and {string} in new password and {string} in confirm password")
+    public void iClickAndITypeInInPasswordAndInNewPasswordAndInConfirmPassword(String arg0, String op, String np, String npc) {
+        getDriver().findElement(By.xpath("//span[contains(text(),'Change Your Password')]")).click();
+        getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(op);
+        getDriver().findElement(By.xpath("//input[@placeholder='New Password']")).sendKeys(np);
+        getDriver().findElement(By.xpath("//input[@placeholder='Confirm New Password']")).sendKeys(npc);
+        getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//button[@class='mat-raised-button mat-primary']")).click();
+
+    }
+
+    @Then("message {string} appears")
+    public void messageAppears(String ts) {
+        WebElement expectedText = getDriver().findElement(By.xpath("//mat-error[@id='mat-error-3',' '"+ ts + "']"));
+        assertThat(expectedText.isDisplayed()).isTrue();
+    }
 
 }
+
+
+
+
 
