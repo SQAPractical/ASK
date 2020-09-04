4e016d1bad2cfab0d040c4e0eeccdf04e30748d5
Naadiya <naadiya@MacBook-Pro.local>
2020-09-03 18:24:53 -0700

    Registration first name from master

--- src/test/java/definitions/registrationFirstName.java
+++ src/test/java/definitions/registrationFirstName.java
@@ -3,26 +3,6 @@ package definitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
-<<<<<<< HEAD
-import static support.TestContext.getDriver;
-public class registrationFirstName {
-    @When("I type first name {string}")
-    public void iTypeFirstName(String firstname) {
-        getDriver().findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(firstname);
-
-    }
-
-    @And("I type last name {string}")
-    public void iTypeLastName(String lastname) {
-
-        getDriver().findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(lastname);
-
-    }
-
-    @And("I type email {string} on registration page")
-    public void iTypeEmailOnRegistrationPage(String email) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(email);
-=======
 
 import static support.TestContext.getDriver;
 
@@ -43,39 +23,16 @@ public class registrationFirstName {
     @And("I type email {string} on registration page")
     public void iTypeEmailOnRegistrationPage(String Email) {
         getDriver().findElement(By.xpath("//input[@placeholder='Email']")).sendKeys((Email));
->>>>>>> master
 
     }
 
     @And("I type group code {string}")
-<<<<<<< HEAD
-    public void iTypeGroupCode(String group) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Group Code']")).sendKeys(group);
-=======
     public void iTypeGroupCode(String Groupcode) {
         getDriver().findElement(By.xpath("//input[@placeholder='Group Code']")).sendKeys(Groupcode);
->>>>>>> master
 
     }
 
     @And("I type password {string} on registration page")
-<<<<<<< HEAD
-    public void iTypePasswordOnRegistrationPage(String password) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
-
-    }
-
-    @And("I type confirm password {string}")
-    public void iTypeConfirmPassword(String ConfirmPassword) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Confirm Password']")).sendKeys(ConfirmPassword);
-
-    }
-
-    @And("I click register me button")
-    public void iClickRegisterMeButton() {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Register Me')]")).click();
-
-=======
     public void iTypePasswordOnRegistrationPage(String Password) {
         getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(Password);
     }
@@ -90,6 +47,5 @@ public class registrationFirstName {
         getDriver().findElement(By.xpath("//span[contains(text(),'Register Me')]")).click();
 
         Thread.sleep(2000);
->>>>>>> master
     }
 }
--- src/test/resources/features/registrationFirstName.feature
+++ src/test/resources/features/registrationFirstName.feature
@@ -1,11 +1,4 @@
 Feature: registration first name
-<<<<<<< HEAD
-  Scenario: registration first name - alpha numeric characters
-    Given I open "registration" page
-    When I type first name "Naadiya123#"
-    And I type last name "Rasheed"
-     And I type email "naadiya.rasheed+auto@gmail.com" on registration page
-=======
 
   Background:
     Given I open "registration" page
@@ -14,15 +7,12 @@ Feature: registration first name
     When I type first name "Jonny1"
     And I type last name "Great"
     And I type email "test@gmail.com" on registration page
->>>>>>> master
     And I type group code "AAA"
     And I type password "12345" on registration page
     And I type confirm password "12345"
     And I click register me button
     Then Text "You have been Registered." appears
 
-<<<<<<< HEAD
-=======
   Scenario Outline: registration first name field is required
     When I type first name <FirstName>
     And I type last name "Great"
@@ -36,6 +26,4 @@ Feature: registration first name
       | FirstName | text                          |
       | ""        | "This field is required"      |
       | " test"   | "Whitespaces are not allowed" |
-
->>>>>>> master
 
