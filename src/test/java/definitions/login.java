f8a15321823b31b4f4cc51a540717a8dbbbd95b8
arysheva <63934238+arysheva@users.noreply.github.com>
2020-09-05 11:23:31 -0700

    resolve conflicts

--- src/test/java/definitions/login.java
+++ src/test/java/definitions/login.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package definitions;
 
 import cucumber.api.java.en.And;
@@ -38,12 +37,6 @@ public class login {
     public void iClickSigninButton() throws InterruptedException {
         getDriver().findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
         Thread.sleep(2000);
-    }
-
-    @Then("text {string} Appears")
-    public void textAppears(String text) {
-        WebElement expectedText = getDriver().findElement(By.xpath("//*[contains(text(),'" + text + "')]"));
-        assertThat(expectedText.isDisplayed()).isTrue();
     }
 
     @Then("Copy and cut options disable, password display in bullets")
@@ -107,34 +100,6 @@ public class login {
         assertThat(expectedText.isDisplayed()).isTrue();
     }
 
-}
-
-
-
-
-
-||||||| merged common ancestors
-=======
-package definitions;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static support.TestContext.getDriver;
-public class login {
-    @Given("I open {string} page")
-    public void iOpenPage(String pageName) {
-        if (pageName.equalsIgnoreCase("login")) {
-            getDriver().get("http://ask-stage.portnov.com/#/login");
-        }
-        else if (pageName.equalsIgnoreCase("registration")) {
-            getDriver().get("http://ask-stage.portnov.com/#/registration");
-        }
-    }
     @When("I type email {string}")
     public void iTypeEmail(String email) {
         getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
@@ -153,14 +118,6 @@ public class login {
         assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'"+text+"')]")).isDisplayed()).isTrue();
     }
 
-
-    @Then("Copy and cut options disable, password display in bullets")
-    public void copyAndCutOptionsDisablePasswordDisplayInBullets() {
-        WebElement passwordField = getDriver().findElement(By.xpath("//input[@placeholder='Password *']"));
-        assertThat(passwordField.getAttribute("type")).isEqualTo("password");
-    }
-
-
     @Then("error message should appear")
     public void errorMessageShouldAppear() {
         assertThat(getDriver().findElement(By.xpath("//*[contains(text(), 'valid email')]")).isDisplayed()).isTrue();
@@ -173,4 +130,4 @@ public class login {
     }
 }
 
->>>>>>> master
+
