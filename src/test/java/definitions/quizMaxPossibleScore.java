08a34af9cc6e02babfe3d504b7851d6b1d3d11aa
arysheva <63934238+arysheva@users.noreply.github.com>
2020-09-05 11:05:35 -0700

    [ADDED] Quiz Maximum Possible Score

--- src/test/java/definitions/quizMaxPossibleScore.java
+++ src/test/java/definitions/quizMaxPossibleScore.java
@@ -15,27 +15,30 @@ import static support.TestContext.getDriver;
 public class quizMaxPossibleScore {
     @When("I click on {string} tab in menu")
     public void iClickOnTabInMenu(String tab) {
+        getDriver().manage().window().maximize();
         getDriver().findElement(By.xpath("//a[contains(@href,'" + tab + "')]")).click();
     }
 
     @And("I create new quiz")
-    public void iCreateNewQuiz() throws InterruptedException {
+    public void iCreateNewQuiz(){
         WebElement spinner = getDriver().findElement(By.xpath("//ac-spinner[@class='ng-star-inserted']"));
         WebDriverWait wait = new WebDriverWait(getDriver(), 5);
         wait.until(ExpectedConditions.visibilityOf(spinner));
         wait.until(ExpectedConditions.invisibilityOf(spinner));
         getDriver().findElement(By.xpath("//span[text()='Create New Quiz']/..")).click();
-        Thread.sleep(4000);
     }
 
     @And("I type Quiz Title {string}")
-    public void iTypeQuizTitle(String title) throws InterruptedException {
+    public void iTypeQuizTitle(String title){
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@formcontrolname = 'name']")));
         getDriver().findElement(By.xpath("//input[@formcontrolname = 'name']")).sendKeys(title);
     }
 
     @And("I click Add Question")
     public void iClickAddQuestion() throws InterruptedException {
         getDriver().findElement(By.xpath("//mat-icon[contains(text(), 'add_circle')]")).click();
+        Thread.sleep(1000);
     }
 
     @And("I choose {string} type")
@@ -43,47 +46,54 @@ public class quizMaxPossibleScore {
         getDriver().findElement(By.xpath("//*[contains(text(), '"+ type +"')]")).click();
     }
 
-    @And("I set {string} points per question")
-    public void iSetPointsPerQuestion(String setPoints) throws InterruptedException {
-        WebElement slider = getDriver().findElement(By.xpath("//div[@class='wrapper']//mat-slider"));
-        WebElement actualPoints = getDriver().findElement(By.xpath("//div[@class='wrapper']//h2"));
-        while (Integer.parseInt(actualPoints.getText()) < Integer.parseInt(setPoints))
-        {
-            slider.sendKeys(Keys.ARROW_RIGHT);
-        }
-
-    }
-
     @And("I save quiz")
     public void iSaveQuiz() {
         getDriver().findElement(By.xpath("//span[contains(text(),'Save')]/..")).click();
-    }
-
-    @And("I wait {int} sec")
-    public void iWaitSec(int sec) throws InterruptedException {
-        Thread.sleep(sec * 1000);
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        WebElement spinner = getDriver().findElement(By.xpath("//ac-spinner"));
+        wait.until(ExpectedConditions.visibilityOf(spinner));
+        wait.until(ExpectedConditions.invisibilityOf(spinner));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.
+                xpath("//h4[contains(text(),'List of Quizzes')]")));
     }
 
     @And("I choose {string} type for question {string}")
     public void iChooseTypeForQuestion(String type, String number) {
-        getDriver().findElement(By.xpath("//*[contains(text(), 'Q"+ number +"')]/../../..//*[contains(text(), '"+ type +"')]")).click();
-
+        getDriver().findElement(By.
+                xpath("//*[contains(text(),'Q" + number + "')]/../../..//*[contains(text(),'" + type + "')]/..")).click();
     }
 
     @And("I type {string} in question {string}")
-    public void iTypeInQuestion(String text, String number) throws InterruptedException {
-        Thread.sleep(2000);
-        getDriver().findElement(By.xpath("//*[contains(text(), 'Q"+ number +"')]/../../..//textarea[@placeholder='Question *']")).sendKeys(text);
-
+    public void iTypeInQuestion(String text, String number){
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        WebElement questionField = getDriver().findElement(By.
+                xpath("//*[contains(text(), 'Q"+ number +"')]/../../..//textarea[@placeholder='Question *']"));
+        wait.until(ExpectedConditions.visibilityOf(questionField));
+        questionField.sendKeys(text);
     }
 
     @And("I set {string} points per question {string}")
     public void iSetPointsPerQuestion(String setPoints, String number) {
-        WebElement slider = getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + number + "')]/../../..//mat-slider"));
-        WebElement actualPoints = getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + number +"')]/../../..//h2"));
-        while (Integer.parseInt(actualPoints.getText()) < Integer.parseInt(setPoints))
+        int setPintsInt = Integer.parseInt(setPoints);
+        WebElement slider = getDriver().findElement(By.
+                xpath("//*[contains(text(), 'Q" + number + "')]/../../..//mat-slider"));
+        WebElement actualPoints = getDriver().findElement(By.
+                xpath("//*[contains(text(), 'Q" + number + "')]/../../..//h2"));
+
+        if (setPintsInt <= 10 && setPintsInt > 5) {
+            while (Integer.parseInt(actualPoints.getText()) < setPintsInt) {
+                slider.sendKeys(Keys.ARROW_RIGHT);
+            }
+        }
+        else if(setPintsInt > 0 && setPintsInt <= 5)
         {
-            slider.sendKeys(Keys.ARROW_RIGHT);
+            while (Integer.parseInt(actualPoints.getText()) > setPintsInt) {
+                slider.sendKeys(Keys.ARROW_LEFT);
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Unsupported value of points!");
         }
 
     }
@@ -98,5 +108,17 @@ public class quizMaxPossibleScore {
     public void iVerifyIfMaximumScoreIsInQuizWithTitle(String score, String title) {
         String xpath = "//*[contains(text(),'"+ title +"')]/../../..//td[contains(text(),'Maximum possible score:')]/following-sibling::td";
         assertThat(getDriver().findElement(By.xpath(xpath)).getText().equals(score)).isTrue();
+    }
+
+    @And("I delete {string} quiz")
+    public void iDeleteQuiz(String QuizName) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        getDriver().findElement(By.
+                xpath("//*[contains(text(), '"+ QuizName +"')]/../../..//*[contains(text(), 'Delete')]")).click();
+        getDriver().findElement(By.
+                xpath("//*[contains(text(), 'Confirmation')]/../..//*[contains(text(), 'Delete')]")).
+                click();
+        wait.until(ExpectedConditions.invisibilityOf(getDriver().
+                findElement(By.xpath("//mat-panel-title[contains(text(),'" + QuizName + "')]/.."))));
     }
 }
--- src/test/resources/features/quizMaxPossibleScore.feature
+++ src/test/resources/features/quizMaxPossibleScore.feature
@@ -1,4 +1,5 @@
 Feature: Maximum possible score
+
   Scenario: Maximum possible score - 2 questions
     Given I open "login" page
     When I type email "teacher3@gmail.com"
@@ -7,7 +8,7 @@ Feature: Maximum possible score
     Then Text "Samy Teacher" appears
     When I click on "quizzes" tab in menu
     And I create new quiz
-    And I type Quiz Title "123456"
+    And I type Quiz Title "3yUfuwDuLmqjoPf"
     And I click Add Question
     And I choose "Textual" type for question "1"
     And I type "How are you today?" in question "1"
@@ -17,9 +18,9 @@ Feature: Maximum possible score
     And I type "What is your name?" in question "2"
     And I set "7" points per question "2"
     And I save quiz
-    And I wait 3 sec
-    And I click on quiz with "123456"
-    Then I verify if maximum score is "16" in quiz with title "123456"
+    And I click on quiz with "3yUfuwDuLmqjoPf"
+    Then I verify if maximum score is "16" in quiz with title "3yUfuwDuLmqjoPf"
+    And I delete "3yUfuwDuLmqjoPf" quiz
 
     Scenario: Quiz - Maximum possible score - 1 question
       Given I open "login" page
@@ -29,12 +30,12 @@ Feature: Maximum possible score
       Then Text "Samy Teacher" appears
       When I click on "quizzes" tab in menu
       And I create new quiz
-      And I type Quiz Title "1234567"
+      And I type Quiz Title "4zgUsivhYZ"
       And I click Add Question
       And I choose "Textual" type for question "1"
       And I type "How are you today?" in question "1"
       And I set "10" points per question "1"
       And I save quiz
-      And I wait 3 sec
-      And I click on quiz with "1234567"
-      Then I verify if maximum score is "10" in quiz with title "1234567"
+      And I click on quiz with "4zgUsivhYZ"
+      Then I verify if maximum score is "10" in quiz with title "4zgUsivhYZ"
+      And I delete "4zgUsivhYZ" quiz
