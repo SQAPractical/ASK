d9aa3f18b7e3f161598e5902ea66ec2832ce4fbb
ruslanDobro <ruslan456@yahoo.com>
2020-10-17 15:02:02 -0400

    multiple

--- src/test/java/definitions/multipleChoiceOther.java
+++ src/test/java/definitions/multipleChoiceOther.java
@@ -1,5 +1,6 @@
 package definitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -10,5 +11,32 @@ public class multipleChoiceOther {
     @When("I click on {string} option button")
     public void iClickOnOptionButton(String text) {
         getDriver().findElement(By.xpath("//*[contains(text(),'"+text+"')]")).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @When("I click on the button Add Question")
+    public void iClickOnTheButtonAddQuestion() {
+        getDriver().findElement(By.xpath("//mat-icon[contains(text(),'add_circle')]")).click();
+
+    }
+
+    @When("I type {string} into Question input field")
+    public void iTypeIntoQuestionInputField(String text) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='question']")).sendKeys(text);
+
+    }
+
+    @And("I type {string} into Question {string} input field")
+    public void iTypeIntoQuestionInputField(String text, String qNumber) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='Option "+qNumber+"*']")).sendKeys(text);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/resources/features/multipleChoiceOther.feature
+++ src/test/resources/features/multipleChoiceOther.feature
@@ -9,4 +9,9 @@ Feature: Multiple Choice Other text area
     When I click on "Quizzes" option button
     And I click on "Create New Quiz" option button
     And I type "Quiz_Test1" into "Title Of The Quiz" input field
-
+    When I click on the button Add Question
+    And I click on "Multiple-Choice" option button  
+    When I type "Select A" into Question input field
+    And I type "A" into Question "1" input field
+    And I type "B" into Question "2" input field
+    When
