73a0735d7ac5b3b2e25af3536f87380a20654bac
SQAPractical <sqapractical@gmail.com>
2020-12-10 15:25:06 -0800

    [IMPROVED] Single Choice Question - Text

--- src/test/java/definitions/singleChoiceQuestionText.java
+++ src/test/java/definitions/singleChoiceQuestionText.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class singleChoiceQuestionText {
@@ -29,10 +30,10 @@ public class singleChoiceQuestionText {
         getDriver().findElement(By.xpath("//*[contains(text(), 'add_circle')]")).click();
     }
 
-    @And("I click {string} radio button")
-    public void iClickRadioButton(String arg0) throws InterruptedException {
-        getDriver().findElement(By.xpath("//label[@class='mat-radio-label']/div[contains(text(), 'Single')]")).click();
-    }
+//    @And("I click {string} radio button")
+//    public void iClickRadioButton(String arg0) throws InterruptedException {
+//        getDriver().findElement(By.xpath("//label[@class='mat-radio-label']/div[contains(text(), 'Single')]")).click();
+//    }
 
     @And("I click {string} filed")
     public void iClickFiled(String arg0) throws InterruptedException {
@@ -43,6 +44,13 @@ public class singleChoiceQuestionText {
     public void iTypeAsQuestion(String qname) throws InterruptedException {
         getDriver().findElement(By.xpath("//*[@placeholder='Question *']")).sendKeys(qname);
         Thread.sleep(2000);
+    }
+
+    @And("I Type {string} as question for question {int}")
+    public void iTypeAsQuestionForQuestion(String qname, int numberOfQuestion) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'Q"+numberOfQuestion+"')]/../../..//*[@placeholder='Question *']")).sendKeys(qname);
+
+
     }
 
     @And("I click {string} answer")
@@ -57,6 +65,15 @@ public class singleChoiceQuestionText {
 
     }
 
+    @And("I Type {string} answer for option {int} for question {int}")
+    public void iTypeAnswerForOptionForQuestion(String textOfOption, int numberOfOption, int numberOfQuestion) throws InterruptedException {
+        Thread.sleep(2000);
+//        getDriver().findElement(By.xpath("//*[contains(text(),'Q"+numberOfQuestion+"')]/../../..//*[@placeholder='Option "+numberOfOption+"*']")).click();
+
+        getDriver().findElement(By.xpath("//*[contains(text(),'Q"+numberOfQuestion+"')]/../../..//*[@placeholder='Option "+numberOfOption+"*']")).sendKeys(textOfOption);
+    }
+
+
     @And("I click option {int}")
     public void iClickOption(int arg0) throws InterruptedException {
         getDriver().findElement(By.xpath("//*[@placeholder='Option 2*']")).click();
@@ -68,10 +85,17 @@ public class singleChoiceQuestionText {
         Thread.sleep(2000);
     }
 
+
     @And("I click radio button correct answer")
     public void iClickRadioButtonCorrectAnswer() throws InterruptedException {
         getDriver().findElement(By.xpath("//*[contains(text(), 'Option 1')]/../../../../../..//mat-radio-button/label")).click();
         Thread.sleep(2000);
+    }
+
+
+    @And("I select option {int} for question {int} as correct answer")
+    public void iSelectOptionForQuestionAsCorrectAnswer(int numberOfOption, int questionNumber) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'Q"+questionNumber+"')]/../../..//*[contains(text(), 'Option "+numberOfOption+"')]/../../../../../..//mat-radio-button/label")).click();
     }
 
     @And("I click Title field")
@@ -91,6 +115,7 @@ public class singleChoiceQuestionText {
 
     @And("I click Save button")
     public void iClickSaveButton() throws InterruptedException {
+        Thread.sleep(2000);
         getDriver().findElement(By.xpath("//span[contains(text(),'Save')]")).click();
         Thread.sleep(2000);
     }
@@ -103,7 +128,14 @@ public class singleChoiceQuestionText {
 
     @And("I delete the question")
     public void iDeleteTheQuestion() throws InterruptedException {
-        getDriver().findElement(By.xpath("//*[@placeholder='Question *']")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+
+        String os = System.getProperty("os.name");
+        if (os.equals("WINDOWS")){
+            Keys.chord(Keys.CONTROL, "a");
+        }else {
+            Keys.chord(Keys.COMMAND, "a");
+        }
+//        getDriver().findElement(By.xpath("//*[@placeholder='Question *']")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         getDriver().findElement(By.xpath("//*[@placeholder='Question *']")).sendKeys(Keys.DELETE);
         getDriver().findElement(By.xpath("//*[@placeholder='Question *']")).sendKeys(Keys.BACK_SPACE);
 
@@ -120,6 +152,30 @@ public class singleChoiceQuestionText {
     @Then("This is too long, should be not more than max characters message appear")
     public void thisIsTooLongShouldBeNotMoreThanMaxCharactersMessageAppear() throws InterruptedException {
         getDriver().findElement(By.xpath("//*[@placeholder='Question *']/../../..//*[contains(@id,'mat-error')][contains(text(),'This is too long')]")).sendKeys();
+        Thread.sleep(2000);
+    }
+
+    @Then("List of Quizzes contain quiz {string}")
+    public void listOfQuizzesContainQuiz(String quizTitle) throws InterruptedException {
+        String listOfQuizzesContent = getDriver().findElement(By.xpath("//div[@class='quizzes']")).getText();
+//        System.out.println(listOfQuizzesContent);
+        assertThat(listOfQuizzesContent.contains(quizTitle)).isTrue();
+        Thread.sleep(2000);
+        System.out.println("List of quizzes contain " + quizTitle);
+    }
+
+
+    @And("I click on quiz {string}")
+    public void iClickOnQuiz(String quizName) throws InterruptedException {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizName+"')]/..")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("I delete quiz {string}")
+    public void iDeleteQuiz(String quizName) throws InterruptedException {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizName+"')]/../../..//*[contains(text(),'Delete')]")).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//*[contains(text(),'Confirmation')]/../..//*[contains(text(),'Delete')]")).click();
         Thread.sleep(2000);
     }
 }
--- src/test/resources/Features/singleChoiceQuestionText.feature
+++ src/test/resources/Features/singleChoiceQuestionText.feature
@@ -9,16 +9,27 @@ Feature: Single Choice Question - Text
     And I click Title field
     And I type "Module 1" for the name of quiz
     And I click "Add Question"
-    And I click "Single Choice" radio button
-    And I click Question filed
-    And I Type "c5&" as question
-    And I click option 1 answer
-    And I Type "A" for answer
-    And I click option 2
-    And I type "B"
-    And I click radio button correct answer
+    And I click "Single-Choice" radio button for question 1
+#    And I click Question filed
+    And I Type "c5&" as question for question 1
+#    And I click option 1 answer
+    And I Type "A" answer for option 1 for question 1
+#    And I click option 2
+    And I Type "B" answer for option 2 for question 1
+    And I select option 1 for question 1 as correct answer
+#    I add second question to Quiz
+    And I click "Add Question"
+    And I click "Single-Choice" radio button for question 2
+    And I Type "c5trgh&" as question for question 2
+    And I Type "C" answer for option 1 for question 2
+#    And I click option 2
+    And I Type "D" answer for option 2 for question 2
+    And I select option 2 for question 2 as correct answer
     And I click Save button
     Then List of Quizzes page loads
+    Then List of Quizzes contain quiz "Module 1"
+    And I click on quiz "Module 1"
+    And I delete quiz "Module 1"
 
   Scenario: Single Choice Question - Text - This field is required
     When I open "Login" page
@@ -30,9 +41,9 @@ Feature: Single Choice Question - Text
     And I click Title field
     And I type "Module 1" for the name of quiz
     And I click "Add Question"
-    And I click "Single Choice" radio button
+    And I click "Single-Choice" radio button
     And I click Question filed
-    And I Type "c5&" as question
+    And I Type "c" as question
     And I delete the question
     Then This field is required message appear
 
@@ -44,7 +55,7 @@ Feature: Single Choice Question - Text
     And I click on "Quizzes" menu item
     And I click on "Create New Quiz" button
     And I click Title field
-    And I type "Module 1" for the name of quiz
+    And I type "Module 2" for the name of quiz
     And I click "Add Question"
     And I click "Single Choice" radio button
     And I click Question filed
@@ -56,6 +67,9 @@ Feature: Single Choice Question - Text
     And I click radio button correct answer
     And I click Save button
     Then List of Quizzes page loads
+    Then List of Quizzes contain quiz "Module 2"
+    And I click on quiz "Module 2"
+    And I delete quiz "Module 2"
 
   Scenario: Single Choice Question - Text - Max 1000 characters
     When I open "Login" page
@@ -77,6 +91,9 @@ Feature: Single Choice Question - Text
     And I click radio button correct answer
     And I click Save button
     Then List of Quizzes page loads
+    Then List of Quizzes contain quiz "Module 1"
+    And I click on quiz "Module 1"
+    And I delete quiz "Module 1"
 
   Scenario: Single Choice Question - Text - Max+1(1001)
     When I open "Login" page
@@ -93,21 +110,3 @@ Feature: Single Choice Question - Text
     And I Type "bmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkbobonb!@#$svevetgetgeggwgtgbmdkboboq" as question
     And I click option 1 answer
     Then This is too long, should be not more than max characters message appear
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
