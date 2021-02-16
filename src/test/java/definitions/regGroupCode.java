3275765f8c04b9cee81d748b0c310ac2f6947c07
SQAPractical <sqapractical@gmail.com>
2021-02-16 14:29:53 -0800

    fixed registration group code

--- src/test/java/definitions/regGroupCode.java
+++ src/test/java/definitions/regGroupCode.java
@@ -10,51 +10,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class regGroupCode {
-    @Given("I navigate to the registration page")
-    public void iNavigateToTheRegistrationPage() {
-        getDriver().get("http://ask-stage.portnov.com/#/registration");
-
-    }
-
-    @When("I type the first name {string}")
-    public void iTypeTheFirstName(String fName) {
-        getDriver().findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(fName);
-    }
-
-    @And("I type the last name {string}")
-    public void iTypeTheLastName(String lName) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(lName);
-    }
-
-    @And("I type the email {string}")
-    public void iTypeTheEmail(String email) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(email);
-    }
-
-    @And("I type the group code {string}")
-    public void iTypeTheGroupCode(String groupCode) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Group Code']")).sendKeys(groupCode);
-    }
-
-    @And("I type the password {string}")
-    public void iTypeThePassword(String password) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
-    }
-
-    @And("I confirm the password {string}")
-    public void iConfirmThePassword(String confirmPassword) {
-        getDriver().findElement(By.xpath("//input[@placeholder='Confirm Password']")).sendKeys(confirmPassword);
-    }
-
-    @And("I click the Register Me button")
-    public void iClickTheRegisterMeButton() throws InterruptedException {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Register Me')]")).click();
-        Thread.sleep(2000);
-    }
-
-    @Then("the Message {string} Appears")
-    public void theMessageAppears(String message) {
-        assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'"+message+"')]")).isDisplayed()).isTrue();
-    }
-
+//    @Given("I navigate to the registration page")
+//    public void iNavigateToTheRegistrationPage() {
+//        getDriver().get("http://ask-stage.portnov.com/#/registration");
+//
+//    }
+//
+//    @When("I type the first name {string}")
+//    public void iTypeTheFirstName(String fName) {
+//        getDriver().findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(fName);
+//    }
+//
+//    @And("I type the last name {string}")
+//    public void iTypeTheLastName(String lName) {
+//        getDriver().findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(lName);
+//    }
+//
+//    @And("I type the email {string}")
+//    public void iTypeTheEmail(String email) {
+//        getDriver().findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(email);
+//    }
+//
+//    @And("I type the group code {string}")
+//    public void iTypeTheGroupCode(String groupCode) {
+//        getDriver().findElement(By.xpath("//input[@placeholder='Group Code']")).sendKeys(groupCode);
+//    }
+//
+//    @And("I type the password {string}")
+//    public void iTypeThePassword(String password) {
+//        getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
+//    }
+//
+//    @And("I confirm the password {string}")
+//    public void iConfirmThePassword(String confirmPassword) {
+//        getDriver().findElement(By.xpath("//input[@placeholder='Confirm Password']")).sendKeys(confirmPassword);
+//    }
+//
+//    @And("I click the Register Me button")
+//    public void iClickTheRegisterMeButton() throws InterruptedException {
+//        getDriver().findElement(By.xpath("//span[contains(text(),'Register Me')]")).click();
+//        Thread.sleep(2000);
+//    }
+//
+//    @Then("the Message {string} Appears")
+//    public void theMessageAppears(String message) {
+//        assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'"+message+"')]")).isDisplayed()).isTrue();
+//    }
+//
 }
--- src/test/resources/features/registrationGroupCode.feature
+++ src/test/resources/features/registrationGroupCode.feature
@@ -1,68 +1,68 @@
 Feature: Registration - Group Code
   Scenario: Group Code - accepts alphanumeric and special characters
-    Given I navigate to the registration page
-    When I type the first name "Ana"
-    And I type the last name "Ivanova"
-    And I type the email "test@test.test"
-    And I type the group code "A$2"
-    And I type the password "12345"
-    And I confirm the password "12345"
-    And I click the Register Me button
-    Then the Message "You have been Registered." Appears
+    Given I navigate to registration page
+    When I type first name "Ana"
+    And I type last name "Ivanova"
+    And I type email "test@test.test"
+    And I type group code "A$2"
+    And I type password "12345"
+    And I confirm password "12345"
+    And I click Register Me
+    Then Message "You have been Registered." Appears
 
   Scenario: Group code - field is required
-    Given I navigate to the registration page
-    Then I type first name "Anna"
+    Given I navigate to registration page
+    When I type first name "Ana"
     And I type last name "Ivanova"
-    And I type email "test123@test.com"
-    And I type the password "12345"
+    And I type email "test@test.test"
+    And I type password "12345"
     And I confirm password "12345"
-    And I click the Register Me button
-    Then the Message "This field is required" Appears
+    And I click Register Me
+    Then Message "This field is required" Appears
 
   Scenario: Group code - minimum 1 character
-    Given I navigate to the registration page
+    Given I navigate to registration page
     And I type first name "Nick"
     And I type last name "Grinch"
     And I type email "test456@test.com"
     And I type group code "A"
     And I type password "12345"
     And I confirm password "12345"
-    Then I click the Register Me button
-    And the Message "You have been Registered." Appears
+    And I click Register Me
+    Then Message "You have been Registered." Appears
 
   Scenario: Group code - maximum 6 characters
-    Given I navigate to the registration page
+    Given I navigate to registration page
     And I type first name "Alex"
     And I type last name "Harry"
     And I type email "test321@test.com"
     And I type password "12345"
     And I confirm password "12345"
     And I type group code "123456"
-    Then I click the Register Me button
-    And the Message "You have been Registered." Appears
+    And I click Register Me
+    Then Message "You have been Registered." Appears
 
   Scenario: Group code - max + 1 characters
-    Given I navigate to the registration page
+    Given I navigate to registration page
     And I type first name "Alex"
     And I type last name "Harry"
     And I type email "test321@test.com"
     And I type password "12345"
     And I confirm password "12345"
     And I type group code "1234567"
-    Then I click the Register Me button
-    And the Message "Too long. Should be no more than 6 characters" Appears
+    And I click Register Me
+    Then Message "Too long. Should be no more than 6 characters" Appears
 
   Scenario: Group code - whitespaces are not allowed
-    Given I navigate to the registration page
+    Given I navigate to registration page
     And I type first name "Alex"
     And I type last name "Harry"
     And I type email "test567@test.com"
     And I type password "12345"
     And I confirm password "12345"
     And I type group code "1 2"
-    Then I click the Register Me button
-    And the Message "Whitespaces are not allowed." Appears
+    And I click Register Me
+    Then Message "Whitespaces are not allowed." Appears
 
 
 
