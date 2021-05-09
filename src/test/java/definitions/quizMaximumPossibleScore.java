980b43678d9fd4482f9ba39dc23ac3dccfc3122b
olafduck <alexpesniak@gmail.com>
2021-05-09 19:38:55 -0400

    added Quiz Maximum Possible Score tests

---
+++ src/test/java/definitions/quizMaximumPossibleScore.java
@@ -0,0 +1,88 @@
+package definitions;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
+
+
+public class quizMaximumPossibleScore {
+    @And("I click on a single-choice radio-button for {int} times in {int} quizzes")
+    public void iClickOnASingleChoiceRadioButtonForTimesInQuizzes(int numberOfTimes, int arg1) {
+        for (int j = 1; j <= numberOfTimes; j++) {
+            getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + j + "')]/..")).click();
+            getDriver().findElement(By.xpath("(//*[contains(text(), 'Single-Choice')]/.)[" + j + "]")).click();
+        }
+    }
+
+    @And("I type {string} in Option {int} and Option {int} in {int} quizzes")
+    public void iTypeOptionsInOptionAndOptionTimesInQuizzes(String quizOption, int firstOption, int secondOption, int numberOfQuizzes) {
+        for (int j = 1; j <= numberOfQuizzes; j++) {
+            getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + j + "')]/..")).click();
+            getDriver().findElement(By.xpath("(//textarea[@placeholder='Option "+firstOption+"*'])[" + j + "]")).sendKeys(quizOption);
+            getDriver().findElement(By.xpath("(//textarea[@placeholder='Option "+secondOption+"*'])[" + j + "]")).sendKeys(quizOption);
+        }
+    }
+
+    @And("I click on option {int} radio-button {int} times in {int} quizzes")
+    public void iClickOnOptionRadioButtonTimesInQuizzes(int arg0, int arg1, int numberOfQuizzes) {
+        for (int j = 1; j <= numberOfQuizzes; j++) {
+            getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + j + "')]/..")).click();
+            getDriver().findElement(By.xpath("(//textarea[@placeholder='Option 1*']/../../../../../mat-radio-button)[" + j + "]")).click();
+        }
+    }
+
+
+    @And("I select a total of {int} points in {string} in {int} questions")
+    public void iSelectATotalOfPointsInInQuestions(int arg0, String arg1, int arg2) throws InterruptedException {
+        int[] sliderOffsetArray = new int[]{0,85,0,33,0,33,33,-10};
+        int numberOfQuestions= 7;
+        for (int j = 1; j <= numberOfQuestions; j++) {
+            getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + j + "')]/..")).click();
+            Thread.sleep(500);
+            WebElement sliderElement = getDriver().findElement(By.xpath("(//*[@class='mat-slider-thumb'])[" + j + "]"));
+            Actions action = new Actions(getDriver());
+            action.dragAndDropBy(sliderElement, sliderOffsetArray[j], 0).perform();
+        }
+    }
+
+    @And("I select {string} in the List of Quizzes")
+    public void iSelectInTheListOfQuizzes(String quizName) throws InterruptedException {
+            Thread.sleep(500);
+            getDriver().findElement(By.xpath("(//*[contains(text(), '"+quizName+"')]/../../..)[last()]")).click();
+    }
+
+
+    @Then("{string} quiz has a maximum possible score of {int}")
+    public void quizHasAMaximumPossibleScoreOf(String quizName, int maxScore) {
+        String maxPointsChosen = "(//*[contains(text(),'"+quizName+"')])[last()]/../../..//table//td[contains(text(), 'Maximum')]/following-sibling::td[text()='" +maxScore+ "']";
+        assertThat(getDriver().findElement(By.xpath(maxPointsChosen)).isDisplayed()).isTrue();
+    }
+
+    @And("I delete {string} from the list of quizzes")
+    public void iDeleteFromTheListOfQuizzes(String quizToDelete) throws InterruptedException {
+        getDriver().findElement(By.xpath("(//*[contains(text(), '"+quizToDelete+"')])[last()]/../..")).click();
+        String deleteButton = "//(*[contains(text(),'"+quizToDelete+"')])[last()]/../../..//button[span[text()='Delete']]";
+        String deleteButtonConfirmation = "//button[@aria-label='Close dialog'][*[contains(text(), 'Delete')]]";
+        getDriver().findElement(By.xpath(deleteButton)).click();
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath(deleteButtonConfirmation)).click();
+
+    }
+
+
+    @And("I select a total of {int} points in {string} in {int} questions version {int}")
+    public void iSelectATotalOfPointsInInQuestionsVersion(int arg0, String arg1, int numberOfQuestions, int arg3) throws InterruptedException {
+        int[] sliderOffsetArray = new int[]{0,0,85,33,0,33};
+        for (int j = 1; j <= numberOfQuestions; j++) {
+            getDriver().findElement(By.xpath("//*[contains(text(), 'Q" + j + "')]/..")).click();
+            Thread.sleep(500);
+            WebElement sliderElement = getDriver().findElement(By.xpath("(//*[@class='mat-slider-thumb'])[" + j + "]"));
+            Actions action = new Actions(getDriver());
+            action.dragAndDropBy(sliderElement, sliderOffsetArray[j], 0).perform();
+        }
+    }
+}
+
---
+++ src/test/resources/features/quizMaximumPossibleScore.feature
@@ -0,0 +1,37 @@
+Feature: Quiz Maximum Possible Score
+  Scenario: Quiz Maximum Possible Score - Seven Questions
+    When I navigate to login page
+    And I type Email "teacher3@gmail.com"
+    And I type Password "12345Abc"
+    And I click on Sign In button
+    And I click on Quizzes button
+    And I click on Create New Quiz button
+    And I type Title Of The Quiz "SQA Quiz 4"
+    And I click on Add Question button for 7 times
+    And I click on a single-choice radio-button for 7 times in 7 quizzes
+    And I type "Quiz" in different question fields 7 times
+    And I type "Answer" in Option 1 and Option 2 in 7 quizzes
+    And I click on option 1 radio-button 7 times in 7 quizzes
+    And I select a total of 45 points in "SQA Quiz 4" in 7 questions
+    And I click on save button
+    And I select "SQA Quiz 4" in the List of Quizzes
+    Then "SQA Quiz 4" quiz has a maximum possible score of 45
+    And I delete "SQA Quiz 4" from the list of quizzes
+
+  Scenario: Quiz Maximum Possible Score - Five Questions
+    When I navigate to login page
+    And I type Email "teacher3@gmail.com"
+    And I type Password "12345Abc"
+    And I click on Sign In button
+    And I click on Quizzes button
+    And I click on Create New Quiz button
+    And I type Title Of The Quiz "SQA Quiz 3"
+    And I click on Add Question button for 5 times
+    And I click on a single-choice radio-button for 5 times in 5 quizzes
+    And I type "Describe Agile model" in different question fields 5 times
+    And I type "It is a group of software development methods" in Option 1 and Option 2 in 5 quizzes
+    And I click on option 1 radio-button 5 times in 5 quizzes
+    And I select a total of 34 points in "SQA Quiz 3" in 5 questions version 2
+    And I click on save button
+    And I select "SQA Quiz 3" in the List of Quizzes
+    Then "SQA Quiz 3" quiz has a maximum possible score of 34
