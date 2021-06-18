5b0654cca9a81b226ccdc3da6604d5c6deee8a92
Nikita <niketheqa@gmail.com>
2021-06-18 09:57:26 -0700

    updated 2

--- src/test/java/definitions/login.java
+++ src/test/java/definitions/login.java
@@ -4,8 +4,11 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
@@ -17,17 +20,23 @@ public class login {
     }
 
     @And("I click on Sign In button")
-    public void iClickOnSignInButton() {
+    public void iClickOnSignInButton() throws InterruptedException {
         getDriver().findElement(By.xpath("//span[contains(.,'Sign In')]")).click();
+        Thread.sleep(2000);
     }
 
     @Then("Element with text {string} is displayed on home page")
-    public void elementWithTextIsDisplayedOnHomePage(String element) {
-        String elementOnHomePage = "//*[contains(text(),'Go To Assignments')]";
-        String xPath = "//*[contains(text(),'"+element+"')]";
-        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(elementOnHomePage)));
-        assertThat(getDriver().findElement(By.xpath(xPath)).isDisplayed()).isTrue();
+    public void elementWithTextIsDisplayedOnHomePage(String footerText) {
+        String xPath = "//footer[@class='padded']";
+        WebElement textElement = getDriver().findElement(By.xpath(xPath));
+        assertThat(textElement.isDisplayed()).isTrue();
+    }
+
+    @Then("Snackbar with text {string} is displayed")
+    public void snackbarWithTextIsDisplayed(String authenticationFailed) {
+        String xPath = "//*[contains(text(),'" + authenticationFailed + "')]";
+        WebElement textElement = getDriver().findElement(By.xpath(xPath));
+        assertThat(textElement.isDisplayed()).isTrue();
     }
 
     @Then("password displays in bullets, copy, cut - disabled")
@@ -38,4 +47,24 @@ public class login {
         String expectedAttributeValue = getDriver().findElement(By.xpath(xPath)).getAttribute(attribute);
         assertThat(expectedAttributeValue).isEqualTo(attributeValue);
     }
+
+    @And("I click on next field")
+    public void iClickOnNextField() throws InterruptedException {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).click();
+        Thread.sleep(2000);
+    }
+
+    @Then("red error message {string} appears underneath")
+    public void redErrorMessageAppearsUnderneath(String errorEmail) {
+        String xPath = "//*[@role='alert'][contains(text(),'" + errorEmail + "')]";
+        WebElement textElement = getDriver().findElement(By.xpath(xPath));
+        assertThat(textElement.isDisplayed()).isTrue();
+    }
+
+    @And("I click on previous field")
+    public void iClickOnPreviousField() throws InterruptedException {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).click();
+        Thread.sleep(2000);
+    }
+
 }
