1a315c75fabea4d4475d6e46b8f51a5dac5a188d
martisha6254 <chi4qa@gmail.com>
2021-06-17 22:58:36 -0700

    ACM-575/+one for change data back

--- src/test/java/definitions/usersmanagement.java
+++ src/test/java/definitions/usersmanagement.java
@@ -1,6 +1,7 @@
 package definitions;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
 
 import static support.TestContext.getDriver;
@@ -48,5 +49,17 @@ public class usersmanagement {
         Thread.sleep(2000);
         getDriver().findElement(By.xpath("//button[@class='mat-button mat-warn'][contains(.,'Log Out')]")).click();
         Thread.sleep(2000);
+    }
+
+    @And("I click on label Teachers")
+    public void iClickOnLabelTeachers() throws InterruptedException {
+        getDriver().findElement(By.xpath("//div[contains(.,'Teachers')]")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("I click on David Davidov name")
+    public void iClickOnDavidDavidovName() throws InterruptedException {
+        Thread.sleep(5000);
+        getDriver().findElement(By.xpath("//*[@class='mat-list-item-content'][contains(.,'David Davidov ')]")).click();
     }
 }
--- src/test/resources/features/usersManagmStRole.feature
+++ src/test/resources/features/usersManagmStRole.feature
@@ -16,3 +16,16 @@ Feature: User's management - Change User's Role
     And I click on Sign In button
     Then Element with text "TEACHER" is displayed on home page
 
+
+    Scenario: Change Teacher's Role to Student Role
+      When I navigate to login page
+      And I type email "teacher1@gmail.com"
+      And I type password "12345Abc"
+      And I click on Sign In button
+      And I click on User's management option
+      And I click on User's management option
+      And I click on label Teachers
+      And I click on David Davidov name
+      And I click on Change User's Role button
+      And I click Change Role button
+
