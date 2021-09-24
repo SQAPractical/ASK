5fc577fd2c83cfa77c512d24dde337175b711b21
Sergii Meln <srgmeln@gmail.com>
2021-09-24 02:10:11 -0400

    [ADDED] A23-268, A23-269, A23-272, A23-275, A23-277, A23-291

--- src/test/java/definitions/usersManagmStName.java
+++ src/test/java/definitions/usersManagmStName.java
@@ -3,26 +3,36 @@ package definitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class usersManagmStName {
     @And("I click Change Your Name button")
-    public void iClickChangeYourNameButton() {
+    public void iClickChangeYourNameButton() throws InterruptedException {
         getDriver().findElement(By.xpath("//span[contains(text(), 'Change Your Name')]")).click();
+        Thread.sleep(2000);
+    }
+
+    @And("I delete name in New Name field")
+    public void iDeleteNameInNewNameField() throws InterruptedException {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.CONTROL + "a");
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.DELETE);
+        Thread.sleep(2000);
+
     }
 
     @And("I type {string} into New Name field")
     public void iTypeIntoNewNameField(String newName) throws InterruptedException {
-        getDriver().findElement(By.xpath("//h1[contains(text(), 'Changing User')]/../../..//input[@placeholder='New name']")).clear();
         getDriver().findElement(By.xpath("//h1[contains(text(), 'Changing User')]/../../..//input[@placeholder='New name']")).sendKeys(newName);
         Thread.sleep(2000);
     }
 
     @And("I click Change button")
-    public void iClickChangeButton() {
+    public void iClickChangeButton() throws InterruptedException {
         getDriver().findElement(By.xpath("//h1[contains(text(), 'Changing User')]/..//span[contains(text(), 'Change')]")).click();
+        Thread.sleep(3000);
     }
 
     @Then("New name {string} is displayed on user's card")
@@ -30,5 +40,21 @@ public class usersManagmStName {
         String name = getDriver().findElement(By.xpath("//mat-card//td[contains(text(), '"+newName+"')]")).getText();
         Thread.sleep(2000);
         assertThat(name.contains(newName)).isTrue();
+    }
+
+    @And("I select and cut existed name from name field")
+    public void iSelectAndCutExistedNameFromNameField() {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.CONTROL + "a");
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.CONTROL + "x");
+    }
+
+    @And("I type Space into New Name field")
+    public void iTypeSpaceIntoNewNameField() {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.SPACE);
+    }
+
+    @And("I paste name back into New Name field")
+    public void iPasteNameBackIntoNewNameField() {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(Keys.CONTROL + "v");
     }
 }
--- src/test/resources/features/usersManagmStName.feature
+++ src/test/resources/features/usersManagmStName.feature
@@ -1,16 +1,61 @@
 Feature: Options in Student's account - Change Name
-  Scenario: Username allows alphanumeric and special characters
+  Background:
     When I navigate to login page
     And I type email "student6@gmail.com"
     And I type password "12345Abc"
     And I click Sign In button
     And I click "Settings" menu item
     And I click Change Your Name button
+
+  Scenario: Username allows alphanumeric and special characters
+    And I delete name in New Name field
     And I type "Irina Ivaniva123@$#" into New Name field
     And I click Change button
     Then New name "Irina Ivaniva123@$#" is displayed on user's card
 # Return back name as it was
     And I click Change Your Name button
+    And I delete name in New Name field
+    And I type "Vladimir Ivanov" into New Name field
+    And I click Change button
+    Then New name "Vladimir Ivanov" is displayed on user's card
+
+  Scenario: Name field can’t be empty
+    And I delete name in New Name field
+    And I click Change button
+    Then Text "This field is required" appears
+
+  Scenario: Name can be 100 characters long
+    And I delete name in New Name field 
+    And I type "Onehundredcharactersssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" into New Name field
+    And I click Change button
+    Then New name "Onehundredcharactersssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" is displayed on user's card
+# Return back name as it was
+    And I click Change Your Name button
+    And I delete name in New Name field
+    And I type "Vladimir Ivanov" into New Name field
+    And I click Change button
+    Then New name "Vladimir Ivanov" is displayed on user's card
+
+  Scenario: Name cannot be more than 100 characters long
+    And I delete name in New Name field
+    And I type "Onehundredonecharactersssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss" into New Name field
+    And I click Change button
+    Then Text "Too long. Should be no more than 100 characters" appears
+
+  Scenario: Name cannot have trailing whitespace character
+    And I type Space into New Name field
+    And I click Change button
+    Then Text "Trailing and leading spaces are not allowed" appears
+
+  Scenario: Name cannot have leading whitespace character
+    And I select and cut existed name from name field
+    And I type Space into New Name field
+    And I paste name back into New Name field
+    And I click Change button
+    Then Text "Trailing and leading spaces are not allowed" appears
+# Return back name as it was
+    And I click Change Your Name button
+    And I delete name in New Name field
     And I type "Vladimir Ivanov" into New Name field
     And I click Change button
     Then New name "Vladimir Ivanov" is displayed on user's card
