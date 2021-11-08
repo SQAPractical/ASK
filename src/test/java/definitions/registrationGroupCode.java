533b36b794bd5a40c1350d17517d08829a6a2855
SQApractical <sqapractical@gmail.com>
2021-11-08 13:46:25 -0800

    fixed conflict in registrationGroupCode.feature and java

--- src/test/java/definitions/registrationGroupCode.java
+++ src/test/java/definitions/registrationGroupCode.java
@@ -12,60 +12,11 @@ import static support.TestContext.getDriver;
 public class registrationGroupCode {
     @When("I navigate to registration page")
     public void iNavigateToRegistrationPage() {
-<<<<<<< HEAD
-=======
 
->>>>>>> master
         getDriver().get("http://ask-stage.portnov.com/#/registration");
     }
 
     @And("I type first name {string}")
-<<<<<<< HEAD
-    public void iTypeFirstName(String FirstName) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname = 'firstName']")).sendKeys(FirstName);
-    }
-
-    @And("I type last name {string}")
-    public void iTypeLasName(String LastName) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname = 'lastName']")).sendKeys(LastName);
-    }
-
-
-    @And("I type email {string}")
-    public void iTypeEmail(String Email) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(Email);
-    }
-
-
-    @And("I type groupcode {string}")
-    public void iTypeGroupcode(String GroupCode) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname='group']")).sendKeys(GroupCode);
-    }
-
-
-    @And("I type password {string}")
-    public void iTypePassword(String Password) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname = 'password']")).sendKeys(Password);
-    }
-
-    @And("I confirm password {string}")
-    public void iConfirmPassword(String ConfirmPassword) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname = 'confirmPassword']")).sendKeys(ConfirmPassword);
-    }
-
-
-    @And("I click Register me button")
-    public void iClickRegisterMeButton() {
-        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
-    }
-
-    @Then("message {string} appears")
-    public void messageAppears(String Message) throws InterruptedException {
-        Thread.sleep(2000);
-        WebElement element = getDriver().findElement(By.xpath("//*[contains(text(), '"+Message+"')]"));
-        assertThat(element.isDisplayed()).isTrue();
-    }
-=======
     public void iTypeFirstName(String fName) {
         getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(fName);
 
@@ -112,5 +63,4 @@ public class registrationGroupCode {
     public void iTypeFirstNameMyFirstName() {
         getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys();
     }
->>>>>>> master
 }
--- src/test/resources/features/registrationGroupCode.feature
+++ src/test/resources/features/registrationGroupCode.feature
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-Feature: Registration - Group Code
-  Scenario: Registration - Group Code - Alphanumeric and sp char
-    When I navigate to registration page
-    And I type first name "Ivan"
-    And I type last name "Ivanov"
-    And I type email "mahmedaboelfe@oreple.com"
-    And I type groupcode "ABDC"
-    And I type password "123456"
-    And I confirm password "123456"
-    And I click Register me button
-    Then message "You have been Registered." appears
-
-
-  Scenario: Registration - Group Code - field is required
-    When I navigate to registration page
-    And I type first name "Ivan"
-    And I type last name "Ivanov"
-    And I type email "mahmedaboelfe@oreple.com"
-    And I type groupcode ""
-    And I type password "123456"
-    And I confirm password "123456"
-    And I click Register me button
-    Then message "Field is required" appears
-=======
 Feature: Registration- Group Code
 
   Background:
@@ -47,6 +22,3 @@ Feature: Registration- Group Code
       | "Test" | "Test" | "smohamedalaa22223@caraparcal.com" | "  1"      | "12345" | "Whitespaces are not allowed" |
       | "Test" | "Test" | "smohamedalaa22223@caraparcal.com" | "1 ds"      | "12345" | "Whitespaces are not allowed" |
       | "Test" | "Test" | "smohamedalaa22223@caraparcal.com" | "1ds "      | "12345" | "Whitespaces are not allowed" |
-
-
->>>>>>> master
