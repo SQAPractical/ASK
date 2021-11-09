b66e2f353a9b737821545718ae95771511f634fe
SQApractical <sqapractical@gmail.com>
2021-11-09 09:32:01 -0800

    Full regression runner

--- src/test/java/Runners/FullRegression.java
+++ src/test/java/Runners/FullRegression.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
         features = "src/test/resources/features",
         glue = {"definitions", "support"},
-        tags = {"@Smoke"} // same as VM option -Dcucumber.options="--tags @predefined"
+        tags = {"@FullRegression"} // same as VM option -Dcucumber.options="--tags @predefined"
 )
 public class FullRegression {
     @BeforeClass
