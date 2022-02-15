f4d0855f3888c3ab7b9095127f97d4c63fe43035
JennyJones01 <epidkevych@gmail.com>
2022-02-15 09:20:02 -0600

    Home Page Buttons

--- src/test/java/definitions/homePageButtons.java
+++ src/test/java/definitions/homePageButtons.java
@@ -2,6 +2,8 @@ package definitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import org.apache.xpath.operations.String;
+import org.assertj.core.api.StringAssert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -9,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class homePageButtons {
+
     @And("I click Go To My Assignments button")
     public void iClickGoToMyAssignmentsButton()  {
         getDriver().findElement(By.xpath("//span[contains(text(),'Go To My Assignments')]")).click();
@@ -23,18 +26,21 @@ public class homePageButtons {
         assertThat(textFromElement.contains("My Assignments")).isTrue();
     }
 
+
     @And("I click on Go To My Grades button")
     public void iClickOnGoToMyGradesButton()  {
         getDriver().findElement(By.xpath("//span[contains(text(),'Go To My Grades')]")).click();
     }
 
-
     @Then("My Grades are present on the page")
     public void myGradesArePresentOnThePage() throws InterruptedException {
+
         String textFromElement = getDriver().findElement(By.xpath("//h4[contains(text(),'My Grades')]")).getText();
+        System.out.println(textFromElement);
         Thread.sleep(2000);
         assertThat(textFromElement.contains("My Grades")).isTrue();
     }
+
 
     @And("I click on  Go To My Submissions button")
     public void iClickOnGoToMySubmissionsButton() {
@@ -42,10 +48,13 @@ public class homePageButtons {
     }
 
     @Then("Submissions are present on the page")
-    public void submissionsArePresentOnThePage() throws InterruptedException  {
-        getDriver().findElement(By.xpath("//h4[contains(text(),'Submissions')]")).getText();
+    public void submissionsArePresentOnThePage() throws InterruptedException {
+        String textFromElement = getDriver().findElement(By.xpath("//h4[contains(text(),'Submissions')]")).getText();
+        System.out.println(textFromElement);
         Thread.sleep(2000);
+        assertThat(textFromElement.contains("Submissions")).isTrue();
     }
+
 
     @And("I click on  Go To Assignments button")
     public void iClickOnGoToAssignmentsButton() {
@@ -54,9 +63,12 @@ public class homePageButtons {
 
     @Then("List of Assignments are present on the page")
     public void listOfAssignmentsArePresentOnThePage() throws InterruptedException {
-        getDriver().findElement(By.xpath("//h4[contains(text(),'List of Assignments')]")).getText();
+        String textFromElement = getDriver().findElement(By.xpath("//h4[contains(text(),'List of Assignments')]")).getText();
+        System.out.println(textFromElement);
         Thread.sleep(2000);
+        assertThat(textFromElement.contains("List of Assignments")).isTrue();
     }
+
 
     @And("I click on Go To Users Management button")
     public void iClickOnGoToUsersManagementButton() {
@@ -65,13 +77,17 @@ public class homePageButtons {
 
     @Then("User's Management is present on the page")
     public void userSManagementIsPresentOnThePage() throws InterruptedException {
-//        getDriver().findElement(By.xpath("/html[1]/body[1]/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-user-management-page[1]/mat-card[1]/h4[1]")).getText();
-        Thread.sleep(2000);
 
-        String xpath = "//h4[contains(text(),'Management')]";
+        java.lang.String xpath = ("//h4[contains(text(),'Management')]");
         WebElement element = getDriver().findElement(By.xpath(xpath));
-
+        Thread.sleep(2000);
         assertThat(element.isDisplayed()).isTrue();
+
+//        String textFromElement = getDriver().findElement(By.xpath("//h4[contains(text(),'User\"'s\"Management')]")).getText();
+//        System.out.println(textFromElement);
+//        Thread.sleep(2000);
+//        assertThat(textFromElement.contains("List of Assignments")).isTrue();
+
 
     }
 }
