0f051f32e8f777a7c5343f72d3a4b5d940db5734
Vladimir Belov <vladimir.belov.qa@gmail.com>
2022-03-29 13:24:59 -0500

    last

--- src/test/java/definitions/MyStepdefs.java
+++ src/test/java/definitions/MyStepdefs.java
@@ -1,8 +1,6 @@
 package definitions;
 
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
 import static support.TestContext.getDriver;
 
@@ -17,4 +15,5 @@ public class MyStepdefs {
     public void testbn() {
         System.out.println("Testing");
     }
+
 }
