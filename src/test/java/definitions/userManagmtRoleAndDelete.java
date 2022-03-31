60605dea253f117ccb7e35270a849da4fbc63370
SQApractical <sqapractical@gmail.com>
2022-03-31 15:55:56 -0700

    Added assertion to User Management - Delete teacher and fixed duplicated step def

--- src/test/java/definitions/userManagmtRoleAndDelete.java
+++ src/test/java/definitions/userManagmtRoleAndDelete.java
@@ -1,8 +1,10 @@
 package definitions;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class userManagmtRoleAndDelete {
@@ -23,11 +25,32 @@ public class userManagmtRoleAndDelete {
         Thread.sleep(2000);
     }
 
-    @And("I click on {string} button")
-    public void iClickOnButton(String userRole) throws InterruptedException {
-        getDriver().findElement(By.xpath("//div//button[contains(.,'"+userRole+"')]")).click();
+    @And("I click on change role")
+    public void iClickOnChangeRole() throws InterruptedException {
+        getDriver().findElement(By.xpath("//div//button[contains(.,'Role')]")).click();
         Thread.sleep(2000);
     }
+
+    @Then("User {string} is not present on list of users")
+    public void userIsNotPresentOnListOfUsers(String userName) {
+        getDriver().findElement(By.xpath("//div[contains(text(),'Teachers')]")).click();
+        String listOfTeachers = getDriver().findElement(By.xpath("//mat-tab-group")).getText();
+        System.out.println(listOfTeachers);
+
+        assertThat(listOfTeachers.contains(userName)).isFalse();
+    }
+
+    @And("I click on Delete User button")
+    public void iClickOnDeleteUserButton() throws InterruptedException {
+        getDriver().findElement(By.xpath("//div//button[contains(.,'Delete')]")).click();
+        Thread.sleep(2000);
+    }
+
+//    @And("I click on {string} button")
+//    public void iClickOnButton(String userRole) throws InterruptedException {
+//        getDriver().findElement(By.xpath("//div//button[contains(.,'"+userRole+"')]")).click();
+//        Thread.sleep(2000);
+//    }
 
 
 
--- src/test/resources/features/usersManagmtRoleAndDelete.feature
+++ src/test/resources/features/usersManagmtRoleAndDelete.feature
@@ -7,13 +7,13 @@ Feature: User Management Role and Delete
     And I click on "Management" on left menu
     And I click on username "eva P"
     And I click on Options button on right side
-    And I click on "Role" button
+    And I click on change role
     And I click on "Change Role" button
     And I click on Options button on right side
-    And I click on "Role" button
+    And I click on change role
     And I click on "Change Role" button
 
-  Scenario: Registration - Group code - Max 6 characters
+  Scenario: User Management - Delete teacher
   # For Delete test case you need to register an account firstly. No need to activate.
     When I navigate to registration page
     And I type first name "Test"
@@ -24,8 +24,6 @@ Feature: User Management Role and Delete
     And I type confirm password "12345Abc"
     And I click on Register Me button
     Then Message "You have been Registered." appears
-
-  Scenario: Delete User Created
   # For Delete test case you need to register an account firstly. No need to activate.
   # Just login with existing teacher, change role for new student to teacher and then delete.
     When I navigate to login page
@@ -35,8 +33,9 @@ Feature: User Management Role and Delete
     And I click on "Management" on left menu
     And I click on username "Test DeleteTestUser"
     And I click on Options button on right side
-    And I click on "Role" button
+    And I click on change role
     And I click on "Change Role" button
     And I click on Options button on right side
-    And I click on "Delete User" button
+    And I click on Delete User button
     And I click on "Delete" button
+    Then User "Test DeleteTestUser" is not present on list of users
