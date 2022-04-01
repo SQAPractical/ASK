7b8fcc1242e3598a56f6fac2019f663d39f205df
nboulatov <nboulatov@gmail.com>
2022-04-01 10:48:36 -0700

    A22-561 update

--- src/test/java/definitions/submissionAutomaticallyGraded.java
+++ src/test/java/definitions/submissionAutomaticallyGraded.java
@@ -1,11 +1,14 @@
 package definitions;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import static support.TestContext.getDriver;
 
 public class submissionAutomaticallyGraded
 {
@@ -16,5 +19,32 @@ public class submissionAutomaticallyGraded
         Actions action = new Actions(driver);
         action.dragAndDropBy(slider, 30, 0).perform();
         Thread.sleep(2000);
+    }
+
+    @And("I select {int} passing percentage")
+    public void iSelectPassingPercentage(int passingPercentage) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        WebElement slider = driver.findElement(By.xpath("//mat-card//h3[contains(text(),'Passing Rate')]/..//div//mat-slider"));
+        Actions action = new Actions(driver);
+        action.dragAndDropBy(slider, 80, 0).perform();
+        Thread.sleep(2000);
+    }
+
+    @And("I type {string} as an answer for question {int}")
+    public void iTypeAsAnAnswerForQuestion(String answer, int questionNumber)
+    {
+        getDriver().findElement(By.xpath("//mat-card//h5[contains(text(),'Question "+questionNumber+"')]/..//textarea")).sendKeys(answer);
+    }
+
+    @And("I select option {int} as an answer for question {int}")
+    public void iSelectOptionAsAnAnswerForQuestion(int optionNumber, int questionNumber)
+    {
+        getDriver().findElement(By.xpath("//mat-card//h5[contains(text(),'Question "+questionNumber+"')]/..//div[@class='mat-radio-label-content'][contains(.,'"+optionNumber+"')]")).click();
+    }
+
+    @Then("I verify {string} status of {string} quiz that was graded at {string}")
+    public void iVerifyStatusOfQuizThatWasGradedAt(String status, String quizName, String gradedAt)
+    {
+        getDriver().findElement(By.xpath("//td/span[contains(text(),'"+status+"')]/../../*[contains(text(),'"+quizName+"')]/../*[contains(text(),'"+gradedAt+"')]"));
     }
 }
--- src/test/resources/features/submissionsAutomaticallyGraded.feature
+++ src/test/resources/features/submissionsAutomaticallyGraded.feature
@@ -10,4 +10,31 @@ Feature: Submissions - Automatically Graded
     And I click on Add question button
     And I select "Textual" question type for question 1
     And I type text "1" as a question for question 1
-    And I select 1 point per question on slider
+    #And I select 1 point per question on slider
+    #And I select 100 passing percentage
+    And I click on Add question button
+    And I select "Single-Choice" question type for question 2
+    And I type text "?" as a question for question 2
+    And I type "1" as option 1 for question 2
+    And I type "2" as option 2 for question 2
+    And I select Option 1 as correct answer for question 2
+    #And I select 10 point per question on slider
+    And I click "Save" button
+    Then Quiz "12345" present in list of quizzes
+    And I click on "Assignments" on left menu
+    And I click on "Create New Assignment" button
+    And I select in dropdown menu option "12345"
+    And I select "Ivan Ivanov" from the list of students
+    And I click on "Give Assignment" button
+    And I logout of teacher account
+    And I type the email "student1@gmail.com"
+    And I type the password "12345Abc"
+    And I click on the Sign in button
+    And I click on "My Assignments" on the left menu
+    And I click on "Go To Assessment" for "12345" quiz
+    And I type "Hello" as an answer for question 1
+    And I select option 2 as an answer for question 2
+    And I click on "Submit My Answers" button
+    And I click on "Ok" button
+    And I click on "My Grades" on the left menu
+    Then I verify "FAILED" status of "12345" quiz that was graded at "Automatic"
